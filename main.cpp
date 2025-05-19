5674330f9191cff674e2036cbc38e6d8e9010ed8
BHouse3 <benjaminhouse@twc.com>
2025-05-18 20:22:58 -0400

    Main function to implement REPL program interface in the future

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 
+#include "core/types.h"
+#include "core/board.h"
+#include "util/debug.h"
+
 int main()
 {
+    std::ofstream myFile("debug.txt");
+
+    cBoard board;
+
+    //board.resetBoard();
+    board.setFromFEN("r1bqk2r/ppp2p2/2n1p2p/8/QbBPn3/2N2pB1/PP3PPP/2R2RK1 b kq - 1 12");
+
+    verifyBoardState(board, myFile);
+
+    std::cout << board.getFEN();
+
+    myFile.close();
 
     return 0;
 }
